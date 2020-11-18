80e670b98bb2d3d01cb546db91a950753faf901c
mingtianyihou33 <liumingyi1428@126.com>
2020-11-18 16:53:39 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <view class="index">
+    <scroll-view scroll-y :scroll-into-view="lastMsgId">
+      <view v-for="item in list" :key="item.id" :id="'msg-'+ item.id">{{item.content}}</view>
+    </scroll-view>
+    <button @click="addItem">添加一条</button>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      list: [],
+      lastMsgId: ''
+    }
+  },
+  methods: {
+    addItem() {
+      this.list.push({id: new Date().getTime(), content: this.list.length})
+      this.scrollToBottom()
+    },
+    scrollToBottom(){
+      this.scrollTo(this.list[this.list.length-1].id)
+    },
+    scrollTo(id) {
+      setTimeout(() => {
+        this.lastMsgId = 'msg-' + id
+      }, 200)
+    },
+  },
+}
+</script>
+
+<style>
+.index {
+  height: 100vh;
+}
+</style>
