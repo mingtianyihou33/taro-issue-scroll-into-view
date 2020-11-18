1976420843cd570cf97492a5a40d20848d23c99f
mingtianyihou33 <liumingyi1428@126.com>
2020-11-18 17:08:09 +0800

    init

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
   <view class="index">
     <scroll-view scroll-y :scroll-into-view="lastMsgId">
-      <view v-for="item in list" :key="item.id" :id="'msg-'+ item.id">{{item.content}}</view>
+      <view class="msg-item" v-for="item in list" :key="item.id" :id="'msg-'+ item.id">content is :{{item.content}} ,id is : {{'msg-'+ item.id}}</view>
     </scroll-view>
-    <button @click="addItem">添加一条</button>
+    <button @tap="addItem">添加一条</button>
   </view>
 </template>
 
@@ -18,6 +18,7 @@ export default {
   },
   methods: {
     addItem() {
+      console.log('addItem')
       this.list.push({id: new Date().getTime(), content: this.list.length})
       this.scrollToBottom()
     },
@@ -37,4 +38,9 @@ export default {
 .index {
   height: 100vh;
 }
+  .msg-item{
+    padding: 10px;
+    border: 1px solid black;
+    margin-bottom: 10px;
+  }
 </style>
